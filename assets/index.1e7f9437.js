286cf5591e1ac3eae7c98efdcd5c6982674ceb19
unknown <mendibass@gmail.com>
2022-04-03 15:52:04 +0300

    deploy to gh-pages

--- assets/index.1e7f9437.js
+++ assets/index.1e7f9437.js
@@ -109,7 +109,7 @@ var T=Object.defineProperty,B=Object.defineProperties;var _=Object.getOwnPropert
     font-weight: 400;
     color: ${e=>e.theme.palette.secondary.main};
     @media (max-width: 600px) {
-      font-size: 50px;
+      font-size: 46px;
     }
   }
 
@@ -119,7 +119,7 @@ var T=Object.defineProperty,B=Object.defineProperties;var _=Object.getOwnPropert
     font-weight: 500;
     color: ${e=>e.theme.palette.secondary.main};
     @media (max-width: 600px) {
-      font-size: 26px;
+      font-size: 22px;
     }
   }
 
